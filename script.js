834f4f6f754e7fe776dd1e69445261c29f55adf0
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-03-29 23:10:20 -0300

    HTML-Avançado.
    Atributos personalizados.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+/*let like = document.getElementById('like');
+
+like.addEventListener('click', function(){
+
+    like.setAttribute('src', 'imagens/dislike.png')
+
+}
+
+
+
+
+let lista = document.getElementById('lista');
+
+let num = lista.getAttribute('num');
+
+let conteudo = ''
+
+for(let i = 0; i < num; i++){
+    conteudo += `<li> número ${i} </li>`
+}
+
+lista.innerHTML = conteudo*/
+
