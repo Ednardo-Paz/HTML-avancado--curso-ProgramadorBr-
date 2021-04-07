8091ae9b0bd60a3ce5d6389b91130dbfb26406d0
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-04-06 23:10:52 -0300

    Canvas HTML - JS
    Canvas HTML - JS

---
+++ animação/canvas/script.js
@@ -0,0 +1,14 @@
+let tela = document.getElementById('tela')
+
+let ctx = tela.getContext('2d')
+/*ctx.moveTo(0,0)
+ctx.lineTo(250, 250)
+ctx.stroke()
+ctx.lineTo(500, 0)
+
+
+ctx.fillStyle = 'blue'
+ctx.fillRect(10,10,100,200)*/
+
+ctx.strokeStyle = 'red'
+ctx.strokeRect(10,10,100,200)
